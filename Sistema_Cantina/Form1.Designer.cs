016e5c01edd69a2eac69a5b2973ccdc772fedb3d
Flavia Martins <flavia.martins1991@hotmail.com>
2021-03-04 15:57:58 -0300

    Complet System, in C#, using windows form

---
+++ Sistema_Cantina/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Sistema_Cantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lstCaixa = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtvalor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(196, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SISTEMA CANTINA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(32, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CÓDIGO DO PRODUTO:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(34, 163);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(262, 31);
+            this.txtCodigo.TabIndex = 2;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.HideSelection = false;
+            this.lstCaixa.Location = new System.Drawing.Point(339, 115);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(421, 253);
+            this.lstCaixa.TabIndex = 3;
+            this.lstCaixa.UseCompatibleStateImageBehavior = false;
+            this.lstCaixa.View = System.Windows.Forms.View.List;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(34, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "VALOR TOTAL:";
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.AutoSize = true;
+            this.txtvalor.Location = new System.Drawing.Point(-138, 116);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(0, 25);
+            this.txtvalor.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 380);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ListView lstCaixa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtvalor;
+    }
+}
+
